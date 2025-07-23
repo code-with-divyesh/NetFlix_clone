9153e913bac2ebdf0fa09a6a7f2c9d4d11b5aadb
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-07-23 12:25:59 +0530

    Added Home page component and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./Home.css";
 import NavBar from "../../components/NavBar/NavBar";
+import { assets } from "../../assets";
 const Home = () => {
   return (
     <div className="home">
       <NavBar />
+      <div className="hero">
+        <img src={assets.hero_banner} className="banner-img" alt="" />
+        <div className="hero-caption">
+          <img src={assets.hero_title} alt="" className="caption-img" />
+          <p>
+            Discover his ties a secret ancient order,a young man living in
+            modern istanbul embarks on a quest to save the city from an immortal
+            enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn">
+              <img src={assets.play_icon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={assets.info_icon} alt="" />
+              More Info
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
